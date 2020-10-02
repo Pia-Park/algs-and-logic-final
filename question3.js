12faafcf637a88e2175f7712f343930613f1647f
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-10-02 10:29:35 -0700

    answer

--- question3.js
+++ question3.js
@@ -10,11 +10,25 @@ what it does and why.
 
 const kebabCase = function (str) {
     // your code here
+    let newStr = str.toLowerCase().split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
+    //make new string(because test number2 has a wierd string...) 
+    // 1. change to lower case and split it 2. using charAt can change first letter to upper case
+    // 3. and plus the left letters(using slice) 4.and! join it again, complete new string! 
+    if(str === '') return 'This is an empty string!';
+    // if str is empty, it returs "Thins is an empty string"   
+    return newStr.replace(/ /gi, '-');
+    // else! it returns new string, this time we have to change space to '-'. 
+    // using replace! you have to plus'gi' after '//' because if you don't plus it, it only changes one time.
 }
+
+
+// console.log((kebabCase('Coding Is Fun'))); 
 
 console.log((kebabCase('Coding Is Fun') === 'Coding-Is-Fun') ? "Test 1: Passing" : "Test 1: Failing");
 
 console.log((kebabCase('this STrIng has Weird casing') === 'This-String-Has-Weird-Casing') ? "Test 2: Passing" : "Test 2: Failing");
 
 console.log((kebabCase('') === 'This is an empty string!') ? "Test 3: Passing" : "Test 3: Failing");
+
+
 
--- question5.js
+++ question5.js
@@ -4,8 +4,20 @@ ex. [2, 3, 5, [1, 2], [3]] ---> 16
 */
 
 const sumNestedArray = function (arr) {
-
+    let result = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(typeof arr[i] == 'number'){
+            result += arr[i];
+        }else if(arr[i] instanceof Array){
+            result += sumNestedArray(arr[i]);
+        }
+    }
+    return result;
 }
+
+
+// console.log((sumNestedArray([1, [2, 3, 4], 6, 7, 5])));
+// console.log((sumNestedArray([1, [2, 3, 4], [2], 7, [3, 5, 6]])));
 
 console.log((sumNestedArray([1, [2, 3, 4], 6, 7, 5]) === 28) ? "Test 1: Passing" : "Test 1: Failing");
 
